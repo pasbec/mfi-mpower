ed6639b78d9b12bcc898389727e272e527d6f73f
pasbec <p.b-dev+mfi-mpower@mailbox.org>
2025-12-16 22:33:14 +0100

    Add security level 1 for cipher

--- src/mfi_mpower/device.py
+++ src/mfi_mpower/device.py
@@ -54,7 +54,7 @@ class MPowerDevice:
         # NOTE: Ubiquiti mFi mPower Devices with firmware 2.1.11 use OpenSSL 1.0.0g (18 Jan 2012)
         if use_ssl:
             self._ssl = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
-            self._ssl.set_ciphers("AES128-SHA")
+            self._ssl.set_ciphers("AES128-SHA:@SECLEVEL=1")
             self._ssl.load_default_certs()
             self._ssl.verify_mode = ssl.CERT_REQUIRED if verify_ssl else ssl.CERT_NONE
         else:
