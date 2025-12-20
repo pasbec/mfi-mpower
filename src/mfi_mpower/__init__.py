b9d20e28e17a8d05cf98fa26b19b242fe5a5102c
pasbec <p.b-dev+mfi-mpower@mailbox.org>
2025-12-20 18:53:26 +0100

    Bump version: 2.3.3 → 2.3.4

--- src/mfi_mpower/__init__.py
+++ src/mfi_mpower/__init__.py
@@ -6,4 +6,4 @@ from .session import MPowerConnectionError, MPowerAuthenticationError, MPowerCom
 from .device import MPowerDevice
 from .entities import MPowerSensor, MPowerSwitch
 
-__version__ = "2.3.3"
+__version__ = "2.3.4"
