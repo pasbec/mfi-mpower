81ffa35f47135a99bb30b320fcbfb15957888236
pasbec <p.b-dev+mfi-mpower@mailbox.org>
2025-12-23 13:57:28 +0100

    Update init

--- src/mfi_mpower/__init__.py
+++ src/mfi_mpower/__init__.py
@@ -2,9 +2,9 @@
 from __future__ import annotations
 
 from .device import MPowerDevice
-from .entities import MPowerSensor, MPowerSwitch
+from .entities import MPowerEntity, MPowerSensor, MPowerSwitch
 from .exceptions import MPowerError, MPowerDataError
-from .interface import MPowerLED
+from .interface import MPowerLED, MPowerIface
 from .session import MPowerConnectionError, MPowerAuthenticationError, MPowerCommandError
 
 __version__ = "2.4.5"
